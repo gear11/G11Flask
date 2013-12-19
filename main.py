6d4e7da7604e70c712ff1eb8c079910125e0964a
Andy Jenkins <andrewbjenkins@gmail.com>
2013-12-19 15:38:47 -0500

    Add decorate proxy mode with template.

--- main.py
+++ main.py
@@ -1,98 +1,117 @@
-""" main.py is the top level script.
-
-Return "Hello World" at the root URL.
 """
+A simple proxy server. Usage:
 
-import sys
+http://hostname:port/p/(URL to be proxied, minus protocol)
 
-# sys.path includes 'server/lib' due to appengine_config.py
-from flask import Flask
-from flask import render_template, request, abort, Response, redirect, url_for
+For example:
+
+http://localhost:8080/p/www.google.com
+
+"""
+from flask import Flask, render_template, request, abort, Response, redirect
 import requests
 import logging
+from filters.custom import json_filter, dpath_filter
+
 app = Flask(__name__.split('.')[0])
-
-#APPROVED_HOSTS = [ "*.google.com", "www.g"]
+logging.basicConfig(level=logging.INFO)
+APPROVED_HOSTS = set(["google.com", "www.google.com", "yahoo.com"])
 CHUNK_SIZE = 1024
-
 LOG = logging.getLogger("main.py")
 
-logging.basicConfig(level=logging.INFO)
+app.jinja_env.filters['json'] = json_filter
+app.jinja_env.filters['dpath'] = dpath_filter
 
-LOG.info("Loading main.py")
+@app.route('/<path:url>')
+def root(url):
+    LOG.info("Root route, path: %s", url)
+    # If referred from a proxy request, then redirect to a URL with the proxy prefix.
+    # This allows server-relative and protocol-relative URLs to work.
+    proxy_ref = proxy_ref_info(request)
+    if proxy_ref:
+        redirect_url = "/p/%s/%s%s" % (proxy_ref[0], url, ("?" + request.query_string if request.query_string else ""))
+        LOG.info("Redirecting referred URL to: %s", redirect_url)
+        return redirect(redirect_url)
+    # Otherwise, default behavior
+    return render_template('hello.html', name=url,request=request)
 
-def is_approved(host):
-    return True
+
+@app.route('/p/<path:url>')
+def proxy(url):
+    """Fetches the specified URL and streams it out to the client.
+
+    If the request was referred by the proxy itself (e.g. this is an image fetch for
+    a previously proxied HTML page), then the original Referer is passed."""
+    r = get_source_rsp(url)
+    LOG.info("Got %s response from %s",r.status_code, url)
+    headers = dict(r.headers)
+    def generate():
+        for chunk in r.iter_content(CHUNK_SIZE):
+            yield chunk
+    return Response(generate(), headers = headers)
+
+
+@app.route('/d/<path:url>')
+def decorate(url):
+    LOG.info("Decorating: %s", url)
+    r = requests.get("http://"+ url, params = request.args)
+    _, host, uri = split_url('http://%s' % url)
+    return render_template('%s.html' % host, text = r.text)
+
+    LOG.info("Received response of type: %s", r.headers.get("content-type"))
+    j = r.json()
+    text = '%s%s%s' % (TOP, j.get("Html"), BOTTOM)
+    return Response(text)
+
+
+def get_source_rsp(url):
+        url = 'http://%s' % url
+        LOG.info("Fetching %s", url)
+        # Ensure the URL is approved, else abort
+        if not is_approved(url):
+            LOG.warn("URL is not approved: %s", url)
+            abort(403)
+        # Pass original Referer for subsequent resource requests
+        proxy_ref = proxy_ref_info(request)
+        headers = { "Referer" : "http://%s/%s" % (proxy_ref[0], proxy_ref[1])} if proxy_ref else {}
+        # Fetch the URL, and stream it back
+        LOG.info("Fetching with headers: %s, %s", url, headers)
+        return requests.get(url, stream=True , params = request.args, headers=headers)
+
+
+def is_approved(url):
+    """Indicates whether the given URL is allowed to be fetched.  This
+    prevents the server from becoming an open proxy"""
+    host = split_url(url)[1]
+    return host in APPROVED_HOSTS
+
 
 def split_url(url):
+    """Splits the given URL into a tuple of (protocol, host, uri)"""
     proto, rest = url.split(':', 1)
-    host, uri = rest[2:].split('/', 1)
+    rest = rest[2:].split('/', 1)
+    host, uri = (rest[0], rest[1]) if len(rest) == 2 else (rest[0], "")
     return (proto, host, uri)
 
 
-#
-# Parses out info indicating the request is from a previously proxied page
-
-# Referer: http://localhost:8080/p/google.com
-#
 def proxy_ref_info(request):
-    ref = request.headers.get('referer')
+    """Parses out Referer info indicating the request is from a previously proxied page.
 
+    For example, if:
+        Referer: http://localhost:8080/p/google.com/search?q=foo
+    then the result is:
+        ("google.com", "search?q=foo")
+    """
+    ref = request.headers.get('referer')
     if ref:
         _, _, uri = split_url(ref)
         if uri.find("/") < 0:
             return None
         first, rest = uri.split("/", 1)
-        if first == ("p"):
+        if first in "pd":
             parts = rest.split("/", 1)
             r = (parts[0], parts[1]) if len(parts) == 2 else (parts[0], "")
             LOG.info("Referred by proxy host, uri: %s, %s", r[0], r[1])
             return r
     return None
 
-
-
-@app.route('/<path:url>')
-def root(url):
-    LOG.info("Root route, path: %s", url)
-    proxy_ref = proxy_ref_info(request)
-
-    if proxy_ref:
-        url = proxy_ref[0] + "/" + url
-        url = url + "?" + request.query_string if request.query_string else url
-        #redirect_url = url_for('proxy', url=url)
-        redirect_url = '/p/%s' % url
-        LOG.info("Redirecting referred URL to: %s", redirect_url)
-        return redirect(redirect_url)
-        #return proxy(proxy_ref[0] + "/" + url)
-
-
-    return render_template('hello.html', name=url,request=request)
-
-
-@app.route('/p/<path:url>')
-def proxy(url):
-    try:
-        url = 'http://%s' % url
-        LOG.info("Proxying %s", url)
-        #host = url.split('/', 1)[0]
-        if not is_approved(url):
-            abort(403)
-
-        proxy_ref = proxy_ref_info(request)
-        headers = { "Referer" : "http://%s/%s" % (proxy_ref[0], proxy_ref[1])} if proxy_ref else {}
-        LOG.info("Fetching with headers: %s, %s", url, headers)
-        r = requests.get(url, stream=True , params = request.args, headers=headers)
-        LOG.info("Got %s response from %s",r.status_code, url)
-        headers = dict(r.headers)
-        #LOG.info("Received response: %s", headers)
-
-        def generate():
-            for chunk in r.iter_content(CHUNK_SIZE):
-                yield chunk
-        resp = Response(generate(), headers = headers)
-        #resp.set_cookie('proxied-host', host)
-        return resp
-    except Exception, ex:
-        LOG.exception(ex)
-        abort(500)
